b5f18e796492c57768d50da6293c74634d7bac53
DJopek <david.jopek@protonmail.com>
2024-10-09 17:58:06 +0200

    finished code for VIII and adjusted a bit style for graphs

--- practicum_II/VIII.py
+++ practicum_II/VIII.py
@@ -1,8 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from linear_fit import fit
+from average import statistics
 
 
+# Data and processing the data
 y_A = []
 f_A = []
 f_12_A = [500, 526, 454, 477, 565, 594, 390, 409, 331, 345]
@@ -10,8 +12,8 @@ C_A = [400, 500, 300, 700, 1000]
 
 y_B = []
 f_B = []
-f_12_B = [501, 530, 427, 459, 543, 581, 359, 388, 306, 326]
-C_B = [400, 500, 300, 700, 1000] 
+f_12_B = [427, 459, 543, 581, 359, 388, 306, 326]
+C_B = [500, 300, 700, 1000] 
 
 y_AB_I = []
 f_AB_I = []
@@ -36,19 +38,30 @@ def average(f_12, y, f):
 
     print(y)
 
+def average_alternative(f_12, y, f):
+    for i in range(8):
+        if i % 2 != 1:
+            f.append((f_12[i]+f_12[i+1])/2)
+
+    print(f)
+
+    for i in range(4):
+        y.append(1/((f[i]*2*3.1415)**2))
+
+    print(y)
+
 average(f_12_A, y_A, f_A)
-fit(y_A, C_A)
+L_A, err_L_A, LC_0_A, err_LC_0_A = fit(y_A, C_A)
 
-# average(f_12_B, y_B, f_B)
-# fit(y_B, C_B)
+average_alternative(f_12_B, y_B, f_B)
+L_B, err_L_B, LC_0_B, err_LC_0_B  = fit(y_B, C_B)
 
-# average(f_12_AB_I, y_AB_I, f_12_AB_I)
-# fit(y_AB_I, C_AB_I)
+average(f_12_AB_I, y_AB_I, f_AB_I)
+L_AB_I, err_L_AB_I, q_AB_I, err_q_AB_I = fit(y_AB_I, C_AB_I)
 
-# average(f_12_AB_II, y_AB_II, f_12_AB_II)
-# fit(y_AB_II, C_AB_II)
+average(f_12_AB_II, y_AB_II, f_AB_II)
+L_AB_II, err_L_AB_II, q_AB_II, err_q_AB_II = fit(y_AB_II, C_AB_II)
 
-# Sample data points
 U_10 = [5, 10, 20, 25, 30, 40, 50, 45, 40, 35, 30, 25, 20, 15]
 f_10 = [382, 390, 397, 399, 401, 404, 407, 465, 470, 478, 487, 503, 520, 588]
 U_100 = [21, 25, 27, 25]
@@ -58,18 +71,23 @@ x = []  # X-coordinates
 y = [] # Y-coordinates
 
 for i in range(14):
-    y.append(U_10[i]/100)
+    y.append(np.sqrt(U_10[i]/100))
     x.append(f_10[i]/449)
 
 for i in range(4):
-    y.append(U_100[i]/30)
+    y.append(np.sqrt(U_100[i]/30))
     x.append(f_100[i]/449)
+
+print("x: ")
+print(x)
+print("y: ")
+print(y)
 
 
 def plot(x,y):
 
     # Create a scatter plot
-    plt.scatter(x, y, color='blue', label='Points')
+    plt.scatter(x, y, color='blue', marker='o', s=35, label='Points')
 
     # Set y-axis limits
     # plt.ylim(0, 2)
@@ -86,7 +104,7 @@ def plot(x,y):
     # plt.show()
 
 # Define the constant d for the custom function
-d = 0.13
+d = 0.231
 
 # Define the custom function for y, using absolute value of the square root
 def custom_function(x):
@@ -105,7 +123,7 @@ plt.figure(figsize=(8, 6))  # Create a figure with a specific size
 plot(x, y)
 
 # Plot the custom function
-plt.plot(x_func, y_func, label=r'$y = |\sqrt{\frac{d^2}{d^2 + \left(x - \frac{1}{x}\right)^2}}|$', color='red', zorder=1)
+plt.plot(x_func, y_func, label=r'$y^2 = {\frac{d^2}{d^2 + \left(x - \frac{1}{x}\right)^2}}$', color='orange', linestyle=':', zorder=1)
 
 # Set labels and title
 plt.xlabel('')
@@ -116,5 +134,61 @@ plt.title('')
 plt.legend()
 
 # Display the plot
-# plt.show()
+plt.show()
 
+# Formulas and errors
+C_0_A = LC_0_A/L_A
+err_LC_0_A = np.sqrt((err_LC_0_A/L_A)**2 + (err_L_A*LC_0_A/(L_A**2))**2)
+
+print("C_0_A: " + str(C_0_A))
+print("err_C_0_A: " + str(err_LC_0_A))
+
+C_0_B = LC_0_B/L_B
+err_LC_0_B = np.sqrt((err_LC_0_B/L_B)**2 + (err_L_B*LC_0_B/(L_B**2))**2)
+
+print("C_0_B: " + str(C_0_B))
+print("err_C_0_B: " + str(err_LC_0_B))
+
+M_I = (L_AB_I - L_A - L_B)/2
+M_II = -(L_AB_II - L_A - L_B)/2
+
+err_M_I = np.sqrt((err_L_AB_I**2 + err_L_A**2 + err_L_B**2)/4)
+err_M_II = np.sqrt((err_L_AB_II**2 + err_L_A**2 + err_L_B**2)/4)
+
+print("M_I: " + str(M_I))
+print("M_II: " + str(M_II))
+print("err_M_I: " + str(err_M_I))
+print("err_M_II: " + str(err_M_II))
+
+M = (L_AB_I - L_AB_II)/4
+err_M = np.sqrt((err_L_AB_I**2 + err_L_AB_II**2)/16)
+
+print("M: " + str(M))
+print("err_M: " + str(err_M))
+
+data = [61.27, 78.72]
+
+M = statistics(data)
+
+print("M average: " + str(M))
+
+data = [402, 496]
+
+average = statistics(data)
+
+print("average: " + str(average))
+
+err_d = 0.025
+
+Q = 1/d
+
+print("Q: " + str(Q))
+
+err_Q = np.sqrt((err_d/d**2)**2)
+print("err_Q: " + str(err_Q))
+
+R = d*np.sqrt(L_AB_II/400)
+err_R = np.sqrt((np.sqrt(L_AB_II/400)*err_d)**2 + (0.5*d/np.sqrt(L_AB_II*400)*err_L_AB_II)**2)
+
+print("R: " + str(R))
+print("err_R: " + str(err_R))
--- practicum_II/linear_fit.py
+++ practicum_II/linear_fit.py
@@ -14,7 +14,7 @@ def fit(data, x):
     # Plotting the data and the linear fit
     plt.scatter(x, data, marker='^', s=100, label='')
     plt.errorbar(x, data, fmt='^', capsize=7)
-    plt.plot(x, predicted, color='orange', linestyle='--', label='')
+    plt.plot(x, predicted, color='orange', linestyle=':', label='')
     plt.xlabel('C [pF]')
     plt.ylabel(r'$\frac{1}{\omega_r^2}\ [s^{2}]$')
     # plt.title('')
@@ -31,7 +31,7 @@ def fit(data, x):
     y_fit=np.polyval(model,x)
     fig,ax=plt.subplots()
     # ax.errorbar(x,data,error_data,marker="o",linewidth=0,elinewidth=2,capsize=5)
-    ax.plot(x,y_fit,c="red")
+    # ax.plot(x,y_fit,c="red")
     ax.set_xlabel("x", fontsize=15)
     ax.set_ylabel("y", fontsize=15)
     # plt.show()
@@ -41,3 +41,5 @@ def fit(data, x):
     print("b = ",model[1], "+/-", np.sqrt(V[1,1]))
     print("cov(a,b) = ",V[0,1])
     print("corr(a,b) = ",V[0,1]/(np.sqrt(V[0,0]*V[1,1])))
+
+    return model[0], np.sqrt(V[0,0]), model[1], np.sqrt(V[1,1])
